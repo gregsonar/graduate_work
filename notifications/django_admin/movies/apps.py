a87f1add45dbbb282ffe6c6ffacb9290c41efb05
Елизавета Рыбкина <elizavetarybkina@homebook.local>
2025-02-09 16:00:06 +0300

    add: refactor notification

--- notifications/django_admin/movies/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class MoviesConfig(AppConfig):
-    name = 'movies'
-    verbose_name = _('Movies')
--- notifications/django_admin/notification/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class NotificationConfig(AppConfig):
-    name = 'notification'
-    verbose_name = _('Notifications')

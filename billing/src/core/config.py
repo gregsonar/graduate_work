19eaa3ae64a92950fb24eb4ff0291e6ebec3ac97
Gregory Dudnikof <1win1@bk.ru>
2025-02-26 00:54:43 +0300

    ref: Параметры конфига получаются из .env (следом за https://github.com/gregsonar/graduate_work/pull/12)

--- billing/src/core/config.py
+++ billing/src/core/config.py
@@ -1,29 +1,38 @@
+import os
+
+from dotenv import load_dotenv
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
+load_dotenv()
+
+print(os.getenv("DB_PASSWORD"))
+
 
 class Setting(BaseSettings):
-    db_host: str = "postgres"
-    db_port: int = 5432
-    db_name: str = "billing_db"
-    db_user: str = "admin"
-    db_password: str = "123qwe"
+    db_name: str = os.getenv("DB_NAME", "billing_db")
+    db_user: str = os.getenv("DB_USER", "admin")
+    db_password: str = os.getenv("DB_PASSWORD", "password")
+    db_host: str = os.getenv("DB_HOST", "postgres")
+    db_port: int = os.getenv("DB_PORT", 5432)
 
-    celery_broker_url: str = "redis://redis_billing:6380/0"
+    celery_broker_url: str = os.getenv("DB_CELERY_BROKER_URL", "redis://redis_billing:6380/0")
 
+    yookassa_shopid: str = Field(os.getenv("YOOKASSA_SHOP_ID"))
     yookassa_token: str = Field(
-        default="test_xB8klULgAEuzogIqiJmKvdKLI5-9SOOTBxFYI6zOjZM"
+        os.getenv("YOOKASSA_API_KEY")
     )
-    yookassa_shopid: str = Field(default="1023840")
 
     check_delay_in_seconds: int = 5
 
     # Используем декоратор Field для явного указания,
     # что это поле должно быть разрешено
     base_url: str = Field(
-        default="http://0.0.0.0/api/subscriptions/api/v1/subscription/"
+        os.getenv("DB_BASE_URL", "http://0.0.0.0/api/subscriptions/api/v1/subscription/")
     )
-    auth_url: str = Field(default="http://auth_api:8000/api/v1/auth/me")
+    auth_url: str = Field(
+        os.getenv("DB_AUTH_URL", "http://auth_api:8000/api/v1/auth/me")
+    )
 
     @property
     def dsn(self) -> str:
